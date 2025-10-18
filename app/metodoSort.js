cdb13d98522bcca943889c991b61c01b9f9468c3
Camila Brito <camilam.brito@outlook.com.br>
2025-10-17 21:00:49 -0300

    Ajustando para ordernar por preco

---
+++ app/metodoSort.js
@@ -0,0 +1,8 @@
+let btnOrdernaPorPreco = document.getElementById("btnOrdenarPorPreco");
+
+btnOrdernaPorPreco.addEventListener("click",ordenarLivrosPorPreco);
+
+function ordenarLivrosPorPreco(){
+    let livrosOrdenados = livros.sort((a,b) => a.preco - b.preco);
+    exibirLivrosNaTela(livrosOrdenados);
+}
